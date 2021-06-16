c9a24ab40e86304a3e05b730d200f7c7e416d132
sifay <74646480+sifay@users.noreply.github.com>
2021-06-15 22:39:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,184 @@
+/*
+let text;
+let rollNumber = 2;
+if(rollNumber === 1){
+   text = "Congratulations You are first";
+} else if(rollNumber === 2){
+  text = "you are second"
+} else if(rollNumber = 3){
+  text = " You are third "
+}
+
+console.log(text);
+ 
+ let weather;
+ let cool = 9
+ if(cool === 7) {
+  weather = "It is a sunny day"
+ } else if(cool === 8){
+  weather = "It is a good weather"
+ } else if(cool === 9){
+  weather = "It is a hot weather"
+ }
+ console.log(weather);
+
+*/
+
+//  1 = one with if else state ments
+/*
+ let hero = prompt("Enter a valid digit:")
+ if(hero == 0)
+  console.log("Zero")
+ else if(hero == 1)
+  console.log("One")
+ else if(hero == 2)
+  console.log("Two")
+ else if(hero == 3)
+  console.log("Three")
+ else if(hero == 4)
+  console.log("Four")
+ else if(hero == 5)
+  console.log("Five")
+ else if(hero == 6)
+  console.log("six")
+ else if(hero == 7)
+  console.log("seven")
+ else if(hero == 8)
+  console.log("Eight")
+ else if(hero == 9)
+  console.log("nine")
+ else if(hero == 10)
+  console.log("Ten")
+ else if(hero == 11)
+  console.log("Eleven")
+ else
+  console.log("enter a valid number")
+*/
+
+
+
+
+      // 0 = zero with switch cases
+// Switch case break default
+/*
+ let game = prompt("Enter a valid Desit:")
+  switch(game) {
+     case "0":
+     console.log("Zero")
+     break;
+     case "1":
+     console.log("One");
+     break;
+  }
+ */
+
+ //let home = prompt("Enter a vowel or consonent")
+/*
+ if(home == "a") {
+  console.log("Vowel")
+ } else if(home == "e") {
+  console.log("Vowel")
+ } else if(home == "i") {
+  console.log("Vowel")
+ } else if(home == "o") {
+  console.log("Vowel")
+ } else if(home == "u") {
+  console.log("Vowel")
+ } else {
+  console.log("consonent")
+ }
+*/
+/*
+ switch(home) {
+    case "a":
+    console.log("Vovel");
+    break;
+    case "e":
+    console.log("Vovel");
+    break;
+    case "i":
+    console.log("Vovel");
+    break;
+    case "o":
+    console.log("Vovel");
+    break;
+    case "u":
+    console.log("Vovel");
+    break;
+    default:
+    console.log("Consonent")
+ }
+
+*/
+
+/*
+  for(let x=3; x<100; x++)
+  {
+    document.write(" " + x)
+  }
+
+*/
+/*
+for(let x = 10;x >= 1; x= x-1) 
+{
+     document.write(" " + x)
+}
+*/
+/*
+ for (let x=1; x<10;x++)
+ {
+     document.write("<h1>HI</h1>")
+ }
+*/
+/*
+ let sum = 0;
+ for(let x = 5;x<=10;x=x+1)
+ {
+    //sum =sum+x;
+    document.write(" " + x)
+ }
+ */
+ //document.write(sum)
+/*
+let = x = 1;
+do{
+    document.write(" "+x)
+    x++;
+} while(x<=10)
+*/
+
+/*
+  let mat = Math.random()
+  console.log(mat*100);
+
+*/
+
+ //  let btn = document.getElementById('btn');
+ //  btn.setAttribute('style', 'color: white;background-color: Red; ')
+
+/*
+ let h22 = document.querySelector('.heaw');
+ h22.innerHTML = "Yoo sifat"
+
+*/
+
+let con = document.querySelector('.container')
+let child = con.children;
+console.log(child) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
